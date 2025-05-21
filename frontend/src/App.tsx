da4a9f647601912826d4a9b6c038f2a4d7459651
Amir Shah <amirshah@Amirs-MacBook-Pro.local>
2025-05-21 18:21:06 +0800

    added link to best moves explanation page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Homepage from './pages/Homepage/Homepage';
 import BoardAnalysis from './pages/BoardAnalysis/BoardAnalysisContent';
 import VsAI from './pages/vsAI/VsAI';
 import WordFinder from './pages/WordFinder/WordFinder';
+import BestMovesExplanation from './pages/BestMovesExplanation/BestMovesExplanation';
 import PageNotFound from './pages/PageNotFound/PageNotFound';
 import './App.css';
 
@@ -15,9 +16,10 @@ export default function App() {
         <main>
           <Routes>
             <Route path="/" element={<Homepage/>} />
-            <Route path="/boardanalysis" element={<BoardAnalysis/>} />
-            <Route path="/vsai" element={<VsAI/>} />
-            <Route path="/wordfinder" element={<WordFinder/>} />
+            <Route path="/board-analysis" element={<BoardAnalysis/>} />
+            <Route path="/vs-ai" element={<VsAI/>} />
+            <Route path="/word-finder" element={<WordFinder/>} />
+            <Route path="/best-moves-explanation" element={<BestMovesExplanation/>} />
             <Route path="*" element={<PageNotFound/>} />
           </Routes>
         </main>
