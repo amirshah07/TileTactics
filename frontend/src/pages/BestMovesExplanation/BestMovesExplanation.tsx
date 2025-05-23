30fa940d7dc2efdc5d2246173b6faab2a8d499ef
Amir Shah <amirshah@Amirs-MacBook-Pro.local>
2025-05-23 19:31:34 +0800

    created page to explain how best moves are calculated

---
+++ frontend/src/pages/BestMovesExplanation/BestMovesExplanation.tsx
@@ -0,0 +1,12 @@
+import './BestMovesExplanation.css';
+
+function BestMovesExplanation() {
+  return (
+    <div className="not-found-container">
+      <h1>How did we calculate the best moves?</h1>
+      <p>Add explanation here</p>
+    </div>
+  );
+}
+
+export default BestMovesExplanation;
