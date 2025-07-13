9c1eedb1d8ddf9feecfd17fa6a8b5f21cff52f5f
amirshah07 <144581429+amirshah07@users.noreply.github.com>
2025-07-13 22:40:49 +0800

    added all multiplier positions

--- backend/internal/board/multipliers.go
+++ backend/internal/board/multipliers.go
@@ -23,6 +23,30 @@ func (b *Board) initializeMultipliers() {
 		b.multipliers[pos[0]][pos[1]] = Multiplier{DoubleWord, 2}
 	}
 
-	// Add triple letter and double letter...
-	// (I can provide full multiplier positions if needed)
+	// Triple letter scores (dark blue squares)
+	tripleLetters := [][2]int{
+		{1, 5}, {1, 9},
+		{5, 1}, {5, 5}, {5, 9}, {5, 13},
+		{9, 1}, {9, 5}, {9, 9}, {9, 13},
+		{13, 5}, {13, 9},
+	}
+	for _, pos := range tripleLetters {
+		b.multipliers[pos[0]][pos[1]] = Multiplier{TripleLetter, 3}
+	}
+
+	// Double letter scores (light blue squares)
+	doubleLetters := [][2]int{
+		{0, 3}, {0, 11},
+		{2, 6}, {2, 8},
+		{3, 0}, {3, 7}, {3, 14},
+		{6, 2}, {6, 6}, {6, 8}, {6, 12},
+		{7, 3}, {7, 11},
+		{8, 2}, {8, 6}, {8, 8}, {8, 12},
+		{11, 0}, {11, 7}, {11, 14},
+		{12, 6}, {12, 8},
+		{14, 3}, {14, 11},
+	}
+	for _, pos := range doubleLetters {
+		b.multipliers[pos[0]][pos[1]] = Multiplier{DoubleLetter, 2}
+	}
 }
