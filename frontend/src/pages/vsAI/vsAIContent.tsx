926e4b9477776d724f539abce48da2483194f2aa
amirshah07 <144581429+amirshah07@users.noreply.github.com>
2025-08-15 17:22:18 +0800

    updated import

--- frontend/src/pages/vsAI/vsAIContent.tsx
+++ frontend/src/pages/vsAI/vsAIContent.tsx
@@ -11,7 +11,7 @@ import { useNoMoreTilesToast } from '../../components/NoMoreTilesToast/useNoMore
 import { loadWasm, analyzeBoard, validateWords as validateWordsWasm } from '../../utils/wasmLoader';
 import { LETTER_DISTRIBUTION, LETTER_VALUES, BOARD_LAYOUT } from '../../utils/constants';
 import type { GameTile, GameState, Move, TileBagState, PlacedTile } from '../../utils/gameTypes';
-import './VsAI.css';
+import './vsAI.css';
 
 // Detect if device supports touch
 const isTouchDevice = () => {
